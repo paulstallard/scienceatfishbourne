06b71d26aa05aba41a6fa392288bc44144e6474e
Paul Stallard <pwastallard@googlemail.com>
2026-01-30 12:03:54 +0000

    Adding details of Feb 26 talk

--- scripts/talkpages.py
+++ scripts/talkpages.py
@@ -270,8 +270,9 @@ will give an overview of how drones are being used across a range of industrial 
 
 next_talk = Talk(
     "2026-02-23",
-    Author("Speaker TBC"),
-    title="Details coming soon",
+    Author("Dr. Ian Goodall"),
+    title="The Magic of the Electron Microscope",
+    subtitle="How Cells Work",
     review_text="Coming soon...",
 )
 
